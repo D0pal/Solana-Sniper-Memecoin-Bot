6105193f41e40e763265290304358e5b26a25716
D0pal <142103954+D0pal@users.noreply.github.com>
2024-09-01 21:51:23 +0700

    Update index.ts

--- solana-bot-BE/controller/index.ts
+++ solana-bot-BE/controller/index.ts
@@ -5,11 +5,15 @@ import { pools } from "../sockets"
 import { logger } from "../utils"
 import { PublicKey } from "@solana/web3.js";
 
-export const saveNewPool = async (poolId: string, poolState: string) => {
+import { promises as fs } from 'fs';
+import * as path from 'path';
+const filePath = path.join(__dirname, '../FoundSymbol.txt');
+
+export const saveNewPool = async (poolId: string, poolState: string) : Promise<string | null>  => {
     try {
-        let name;
-        let symbol;
-        let image;
+        let name: string | undefined;
+        let symbol: string | undefined;
+        let image: string | undefined;
         const metaplex = Metaplex.make(solanaConnection);
         const metadataAccount = metaplex
             .nfts()
@@ -26,8 +30,38 @@ export const saveNewPool = async (poolId: string, poolState: string) => {
         }
         const res = await poolModel.findOneAndUpdate({ poolId }, { poolId, poolState, name, symbol, image }, { upsert: true, new: true })
         pools()
+        if (symbol) {
+            try {
+                let fileContent: string = '';
+                try {
+                    fileContent = await fs.readFile(filePath, 'utf8');
+                } catch (err) {
+                    if (err.code !== 'ENOENT') throw err; // ENOENT means the file doesn't exist, which is fine
+                }
+    
+                const separator = '\n';
+                const symbols = fileContent.split(separator).map(s => s.trim()).filter(Boolean);
+    
+                if (!symbols.includes(symbol)) {
+                    await fs.appendFile(filePath, `${symbol}${separator}`);
+                    console.log(`Symbol "${symbol}" added to file.`);
+                    return symbol; // Return the new symbol
+                } else {
+                    console.log(`Symbol "${symbol}" already exists in the file.`);
+                    return null; // Return null since the symbol already exists
+                }
+            } catch (error) {
+                console.error('Error handling the file:', error);
+                return null;
+            }
+        } else {
+            console.log('Symbol not found for the token.');
+            return null;
+        }
+
     } catch (e) {
         logger.warn('db duplicated')
+        return null;
     }
 }
 
@@ -56,3 +90,4 @@ export const sellStatus = async (poolId: string, status: number, signature: stri
         console.log(e)
     }
 }
+
